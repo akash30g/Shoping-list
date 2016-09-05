decaffe2503b41e31a5d0a47915acdb0360d20c0
akash gupta <akash30g@gmail.com>
2016-09-05 11:16:25 +0800

    add items function added

--- AkashGuptaA1.py
+++ AkashGuptaA1.py
@@ -5,7 +5,7 @@ import csv
 def getUserName():  # Asks the name from the user and saves it.
     global name
     name = input("Please enter your name:")
-    while name == '' or len(name) > 27:
+    while name == '' or len(name) > 15:
         print("Your name cannot be blank and must be <= 15 characters")
         name = input("Please enter your name:")
     return name
@@ -21,7 +21,7 @@ Q - Quit. \n\
 >>>")
     return menuChoice
 
-def readList(shopItem):
+def readList_required(shopItem,numline):
     count = 0
     output_info = "Required items:\n"
     totalPrice = 0
@@ -31,48 +31,85 @@ def readList(shopItem):
                                                        every_product["Priority"])
         count =count+1
         totalPrice += every_product["Price"]
+    print("{} items loaded from items.csv".format(len(shopItem)))
     print(output_info + "Total expected price for {} items: $ {}".format(count, totalPrice))
 
     return shopItem
 
-def writeList():
-    file_write = open('list.txt', 'w')
-    for each_line in file_write:
-        datum = each_line.split(",")
-        file_write.write("{:s}\t:${:s}\t({:s})".format(datum[0], datum[1], datum[2], datum[3]))
-    file_write.close()
+
+def writelist(shopItem):
+    checkProductPrice= True
+    checkProductPriority = True
+    product_name = input("Please enter the product name:").capitalize()
+    while product_name == '' or len(product_name) > 15:
+        print("the item name cannot be blank and must be < 15 characters")
+        product_name = input("Please enter the product name:").capitalize()
+    while checkProductPrice:
+
+        try:
+            product_price = int(input("Price: $"))
+            if product_price < 0:
+                print("Price must be >= 0")
+            else:
+                checkProductPrice = False
+        except ValueError:
+            print("Invalid input; Please, enter a valid number")
 
 
-def main():  # main function of the program shopping list
+    while checkProductPriority:
+
+        try:
+            product_priority = int(input("Priority:"))
+            if product_priority < 0 or product_priority > 3:
+                print("Priority must be 1, 2 or 3")
+            else:
+                checkProductPriority = False
+
+
+        except ValueError:
+            print("Invalid input; Please, enter a valid number")
+    added_item= {"Name": product_name, "Price": product_price, "Priority": product_priority}
+    shopItem.append(added_item)
+
+    return shopItem
+
+
     print("Shopping List 1.0 - by Akash Gupta")
     shopItem = []
+    listCompleted = []
     getUserName()
     print("Hi {} Let's go for shopping!!!".format(name))
-
+    numline = sum(1 for line in open("items.csv", "r"))
+    file = open("items.csv", "r")
+    for each_line in file:
+        datum = each_line.split(",")
+        Item = {"Name": datum[0], "Price": float(datum[1]), 'Priority': int(datum[2]), "Type": datum[3]}
+        shopItem.append(Item)
     while True:
         Choice = Menu()
         if Choice == 'r' or Choice == 'R':
-            numline = sum(1 for line in open("items.csv","r"))
-            print("{} items loaded from items.csv".format(numline))
-            file = open("items.csv", "r")
-            for each_line in file:
-                datum = each_line.split(",")
-                Item = {"Name": datum[0], "Price": float(datum[1]), 'Priority': int(datum[2]), "Type": datum[3]}
 
-                shopItem.append(Item)
-            readList(shopItem)
+            readList_required(shopItem,numline)
 
-        #elif Choice == 'c' or Choice == 'C':
+        elif Choice == 'c' or Choice == 'C':
+            readList_completed(listCompleted)
 
         elif Choice == 'a' or Choice == 'A':
-            writeList()
-        #elif Choice == 'm' or Choice == 'M':
-
-        #elif Choice == 'q' or Choice == 'Q':
-
-           # break
+            writelist(shopItem)
+        elif Choice == 'm' or Choice == 'M':
+            deleteitem = markProduct(shopItem)
+            shopItem.remove(deleteitem)
+            listCompleted.append(deleteitem)
+        elif Choice == 'q' or Choice == 'Q':
+            print("Thanks for shopping \nHave a nice day!! :)")
+            break
         else:
             print("Invalid menu choice. Please try again.")
-
+    addcsvList(shopItem)
 
 main()  # calls the main function
+
+
+
+
+
